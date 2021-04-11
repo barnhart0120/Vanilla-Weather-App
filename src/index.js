9da877d03a746b6be7d6a1086e9be33186843756
Danielle <78396825+barnhart0120@users.noreply.github.com>
2021-04-11 14:53:39 -0700

    Added styling around current date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-let apiKey = `1b0ea497e82983c26b919b587144daf8`;
-let city = "Portland";
-let units = `imperial`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = `${day}<br /> ${month} ${date}, ${year}`;
 
 function displayWeather(response) {
-  console.log(response.data);
   let currentTemp = document.querySelector("#current-temp");
   currentTemp.innerHTML = Math.round(response.data.main.temp);
 
@@ -15,10 +42,10 @@ function displayWeather(response) {
   currentCondition.innerHTML = response.data.weather[0].main;
 
   let lowTemp = document.querySelector("#low-temp");
-  lowTemp.innerHTML = response.data.main.temp_min;
+  lowTemp.innerHTML = Math.round(response.data.main.temp_min);
 
   let highTemp = document.querySelector("#high-temp");
-  highTemp.innerHTML = response.data.main.temp_max;
+  highTemp.innerHTML = Math.round(response.data.main.temp_max);
 
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
@@ -26,4 +53,9 @@ function displayWeather(response) {
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
+let apiKey = `1b0ea497e82983c26b919b587144daf8`;
+let city = "Portland";
+let units = `imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
 axios.get(apiUrl).then(displayWeather);
