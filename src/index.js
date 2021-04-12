2ea87c040f04bcfc58daf412c3b2281a0e9fcbe4
Danielle <78396825+barnhart0120@users.noreply.github.com>
2021-04-12 14:36:20 -0700

    Added function for search bar in js

--- src/index.js
+++ src/index.js
@@ -31,9 +31,11 @@ let year = now.getFullYear();
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${day}<br /> ${month} ${date}, ${year}`;
 
-function showFullMinutes() {
-  let hour = now.getHours();
+function displayTime() {
   let minutes = now.getMinutes();
+  let hour = now.getHours();
+  let lateHour = hour - 12;
+
   if (minutes < 10) {
     let currentTime = document.querySelector("#time");
     currentTime.innerHTML = `${hour}:0${minutes}`;
@@ -41,6 +43,7 @@ function showFullMinutes() {
     let currentTime = document.querySelector("#time");
     currentTime.innerHTML = `${hour}:${minutes}`;
   }
+
   if (hour >= 12) {
     let timeFormat = document.querySelector("#am-pm");
     timeFormat.innerHTML = `PM`;
@@ -48,8 +51,13 @@ function showFullMinutes() {
     let timeFormat = document.querySelector("#am-pm");
     timeFormat.innerHTML = `AM`;
   }
+
+  if (hour > 12) {
+    let currentTime = document.querySelector("#time");
+    currentTime.innerHTML = `${lateHour}:${minutes}`;
+  }
 }
-showFullMinutes();
+displayTime();
 
 function displayWeather(response) {
   let currentTemp = document.querySelector("#current-temp");
@@ -59,7 +67,7 @@ function displayWeather(response) {
   cityName.innerHTML = response.data.name;
 
   let currentCondition = document.querySelector("#weather-condition");
-  currentCondition.innerHTML = response.data.weather[0].main;
+  currentCondition.innerHTML = response.data.weather[0].description;
 
   let lowTemp = document.querySelector("#low-temp");
   lowTemp.innerHTML = Math.round(response.data.main.temp_min);
@@ -73,9 +81,20 @@ function displayWeather(response) {
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
-let apiKey = `1b0ea497e82983c26b919b587144daf8`;
-let city = "Portland";
-let units = `imperial`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(displayWeather);
+function searchCity(city) {
+  let apiKey = `1b0ea497e82983c26b919b587144daf8`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-bar");
+  searchCity(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+searchCity("Rome");
