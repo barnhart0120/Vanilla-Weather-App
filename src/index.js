d6a45b46b0851c64fb3247bed7cb4fc6eadbb605
Danielle <78396825+barnhart0120@users.noreply.github.com>
2021-04-12 16:28:09 -0700

    Added Fahrenheit link

--- src/index.js
+++ src/index.js
@@ -36,12 +36,18 @@ function displayTime() {
   let hour = now.getHours();
   let lateHour = hour - 12;
 
-  if (minutes < 10) {
-    let currentTime = document.querySelector("#time");
-    currentTime.innerHTML = `${hour}:0${minutes}`;
-  } else {
+  if (minutes > 10 && hour < 12) {
     let currentTime = document.querySelector("#time");
     currentTime.innerHTML = `${hour}:${minutes}`;
+  } else if (minutes < 10 && hour > 12) {
+    let currentTime = document.querySelector("#time");
+    currentTime.innerHTML = `${lateHour}:0${minutes}`;
+  } else if (minutes > 10 && hour > 12) {
+    let currentTime = document.querySelector("#time");
+    currentTime.innerHTML = `${lateHour}:${minutes}`;
+  } else if (minutes < 10 && hour < 12) {
+    let currentTime = document.querySelector("#time");
+    currentTime.innerHTML = `${hour}:0${minutes}`;
   }
 
   if (hour >= 12) {
@@ -50,11 +56,6 @@ function displayTime() {
   } else {
     let timeFormat = document.querySelector("#am-pm");
     timeFormat.innerHTML = `AM`;
-  }
-
-  if (hour > 12) {
-    let currentTime = document.querySelector("#time");
-    currentTime.innerHTML = `${lateHour}:${minutes}`;
   }
 }
 displayTime();
@@ -128,5 +129,37 @@ function convertToCelsius(event) {
 }
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = document.querySelector("#current-temp");
+  let temperature = fahrenheitTemp.innerHTML;
+  fahrenheitTemp.innerHTML = Math.round(temperature * 1.8 + 32);
+
+  let tempUnit = document.querySelector(".temp-unit");
+  tempUnit.innerHTML = "F";
+
+  let highTemp = document.querySelector("#high-temp");
+  let highTempFahrenheit = highTemp.innerHTML;
+  highTemp.innerHTML = Math.round(highTempFahrenheit * 1.8 + 32);
+
+  let lowTemp = document.querySelector("#low-temp");
+  let lowTempFahrenheit = lowTemp.innerHTML;
+  lowTemp.innerHTML = Math.round(lowTempFahrenheit * 1.8 + 32);
+
+  let lowUnit = document.querySelector(".low-unit");
+  lowUnit.innerHTML = "F";
+  let highUnit = document.querySelector(".high-unit");
+  highUnit.innerHTML = "F";
+
+  let windSpeed = document.querySelector("#wind-speed");
+  let windSpeedImperial = windSpeed.innerHTML;
+  windSpeed.innerHTML = Math.round(windSpeedImperial / 1.609);
+
+  let windUnit = document.querySelector(".wind-unit");
+  windUnit.innerHTML = " mph";
+}
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 searchCity("Rome");
